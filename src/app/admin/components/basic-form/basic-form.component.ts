46c42d86610bd777b5ae26339c8a1aeed72630ea
Nicolas Molina Monroy <hi@nicobytes.com>
2020-08-25 12:08:05 -0400

    :construction: implement form builder

--- src/app/admin/components/basic-form/basic-form.component.ts
+++ src/app/admin/components/basic-form/basic-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, Validators, FormGroup } from '@angular/forms';
+import { FormControl, Validators, FormGroup, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-basic-form',
@@ -8,24 +8,20 @@ import { FormControl, Validators, FormGroup } from '@angular/forms';
 })
 export class BasicFormComponent implements OnInit {
 
-  form = new FormGroup({
-    name: new FormControl('', [Validators.required, Validators.maxLength(10)]),
-    email: new FormControl(''),
-    phone: new FormControl(''),
-    color: new FormControl('#000000'),
-    date: new FormControl(''),
-    age: new FormControl(12),
-    category: new FormControl(''),
-    tag: new FormControl(''),
-    agree: new FormControl(false),
-    gender: new FormControl(''),
-    zone: new FormControl(''),
-  });
+  form: FormGroup;
 
-  constructor() { }
+  constructor(
+    private formBuilder: FormBuilder
+  ) {
+    this.buildForm();
+  }
 
   ngOnInit(): void {
     this.nameField.valueChanges
+    .subscribe(value => {
+      console.log(value);
+    });
+    this.form.valueChanges
     .subscribe(value => {
       console.log(value);
     });
@@ -36,7 +32,27 @@ export class BasicFormComponent implements OnInit {
   }
 
   save(event) {
-    console.log(this.form.value);
+    if (this.form.valid){
+      console.log(this.form.value);
+    } else {
+      this.form.markAllAsTouched();
+    }
+  }
+
+  private buildForm() {
+    this.form = this.formBuilder.group({
+      name: ['', [Validators.required, Validators.maxLength(10)]],
+      email: [''],
+      phone: ['', Validators.required],
+      color: ['#000000'],
+      date: [''],
+      age: [12],
+      category: [''],
+      tag: [''],
+      agree: [false],
+      gender: [''],
+      zone: [''],
+    });
   }
 
   get nameField() {
