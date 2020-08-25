d2724a7d5456a5186707f4f6ed5fdbcb58d18710
Nicolas Molina Monroy <hi@nicobytes.com>
2020-08-25 12:31:01 -0400

    :construction: work in validators

--- src/app/admin/components/basic-form/basic-form.component.ts
+++ src/app/admin/components/basic-form/basic-form.component.ts
@@ -41,15 +41,15 @@ export class BasicFormComponent implements OnInit {
 
   private buildForm() {
     this.form = this.formBuilder.group({
-      name: ['', [Validators.required, Validators.maxLength(10)]],
-      email: [''],
+      name: ['', [Validators.required, Validators.maxLength(10), Validators.pattern(/^[a-zA-Z ]+$/)]],
+      email: ['', [Validators.required, Validators.email]],
       phone: ['', Validators.required],
       color: ['#000000'],
       date: [''],
-      age: [12],
+      age: [18, [Validators.required, Validators.min(18), Validators.max(100)]],
       category: [''],
       tag: [''],
-      agree: [false],
+      agree: [false, [Validators.requiredTrue]],
       gender: [''],
       zone: [''],
     });
